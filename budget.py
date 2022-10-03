6ba29ea79971574cc36cdb60e2853db889223f16
ksenek <110329390+ksenek@users.noreply.github.com>
2022-10-04 10:03:11 +1300

    Added test print, added test comment, deleted print statement

--- budget.py
+++ budget.py
@@ -7,4 +7,8 @@ budget = 66
 people = 12
 eat_lunch = budget/people
 print("$ " + str(budget) + " for", people," people")
-print("Each person can spend ", eat_lunch)
+
+
+print("Test comment")
+
+#test comment
