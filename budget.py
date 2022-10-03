61c8723b9f468361f732c85c0833130a1ab80ce4
ksenek <110329390+ksenek@users.noreply.github.com>
2022-10-04 10:00:57 +1300

    Initial file with base code

---
+++ budget.py
@@ -0,0 +1,10 @@
+##
+# budget.py
+# Your name
+# Todays date
+
+budget = 66
+people = 12
+eat_lunch = budget/people
+print("$ " + str(budget) + " for", people," people")
+print("Each person can spend ", eat_lunch)
